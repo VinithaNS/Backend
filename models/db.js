da039bb63f0d5ee4b422cda8205a7b774d15cb7c
vinithaneyashree <vinithaneyashree2901@gmail.com>
2022-06-14 16:25:53 +0530

    backend added

---
+++ models/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const url=process.env.MONGO_URL;
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
